9b168dac7e6967dac1cac2e295c4258f52afc8ef
Ravi S. Singh <ravi.ss@imerit.net>
2021-02-09 16:46:28 +0530

    material ui library and mocking server added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Default from "./containers/default";
 
-function App() {
+
+import Dashboard from "./screens/Dashboard";
+import Products from "./screens/Products";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+
+          <Route path="/products">
+            <Default>
+              <Products />
+            </Default>
+          </Route>
+          <Route path="/">
+            <Default>
+              <Dashboard />
+            </Default>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/mock-server.js
@@ -0,0 +1,18 @@
+import { Response, Server } from "miragejs"
+// Create a new server instance - Intercepts the requests
+if (1 || process.env.NODE_ENV === "development") {
+    const server = new Server({
+        urlPrefix: "http://localhost:8000/",
+        namespace: "",
+        routes() {
+            this.get("/countries/:id", (schema, request) => {
+                let { id } = request.params;
+                switch (id) {
+                    case "1":
+                        return new Response(404, { "Content-Type": "application/json" }, { error: 'Country not found' });
+                }
+            });
+        },
+    });
+    server.passthrough();
+}
